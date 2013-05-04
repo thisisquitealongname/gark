3e33d03d44b20eec8a07e7ba1781a086abd68d7c
Julien Bergeron <jbergero@csclub.uwaterloo.ca>
2013-05-04 17:41:21 -0400

    an update, because Loren asked me to

--- jbergero/twine_parser.rb
+++ jbergero/twine_parser.rb
@@ -1,22 +1,45 @@
+@master_node_list = []
+
+#buggy atm :(
 class TwineParselet
   
+ # my_text
   
+  def initialize
+    my_text = ''
+  end
   
   #cram another line of .tw text into here
   def cram line
-    
+    my_text << line.strip
   end
   
+  #okay... now that we have all our data in my_text, it's time to start parsing
+  def wrapup mnl
+    mnl << self
+  end
   
 end
 
 
 
-File.open("./gark/hamster.txt", "r") do |infile|
+File.open("../hamster.txt", "r") do |infile|
   node = TwineParselet.new
   while (line = infile.gets)
-    node = TwineParselet.new if line.index "::"
+    if (line.index "::")
+      node.wrapup @master_node_list
+      node = TwineParselet.new
+    end
     node.cram line
   end
+  node.wrapup @master_node_list
 end
   
+@master_node_list.each do |x|
+  
+  puts x
+  puts
+  puts '========'
+  puts
+  
+end
