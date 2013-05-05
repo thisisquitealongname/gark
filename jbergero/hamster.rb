7f6ff12146b197cada3cbda200c7d1c25cf8eb3e
unknown <thisisquitealongname@gmail.com>
2013-05-05 19:52:04 -0400

    FUCK

--- jbergero/hamster.rb
+++ jbergero/hamster.rb
@@ -4,119 +4,107 @@ sex
 @hamster_node_list = []
 l = @hamster_node_list
 
-l << {
-:name => 'Start',
-:text => %(It's morning. You want to explore.. but you've chewed all these bars before. You nibble at the same chunk of sawdust you peed on a month ago. Your life is one long, tediuous game of waiting, punctuated by terrifying moments of being swung up into the arms of your owners while they parrot the word "Squiggles". Confinement is second-nature -- it's almost as if you're playing a minor character in your own story.
+page :start,
+	%^It's morning. You want to explore.. but you've chewed all these bars before. You nibble at the same chunk of sawdust you peed on a month ago. Your life is one long, tediuous game of waiting, punctuated by terrifying moments of being swung up into the arms of your owners while they parrot the word "Squiggles". Confinement is second-nature -- it's almost as if you're playing a minor character in your own story.
 
-You're in one of the few places you know.),
-:code => '
-	$hamster_cage_open = true
-	$hamster_cage_destroyed = false
-	$cat_location = :bedroom
-	$bedroom_occupied = false
-	',
-:options => [:incage]
-}
-
-l << {
-:name => 'In Cage',
-:text => "You're in your cage. The door is ",
-:code => '
-	if $hamster_cage_open
-	  @text << "open"
-	  @options << :exitcage
-	else
-	  @text << "closed"
+	You're in one of the few places you know.^,
+	:location => :cage,
+	:incage => "You're in your cage",
+	do
+		$hamster_cage_open = true
+		$hamster_cage_destroyed = false
+		$cat_location = :bedroom
+		$bedroom_occupied = false
 	end
-	',
-:options => [:stayincage]
-}
 
-l << {
-:name => 'Exit Cage',
-:text => "You scurry out the door. On the last bar, you realize there's nothing stopping your 4 foot descent onto the tile floor. You try to cling onto metal with your hind legs, but it's no use. You plummet.
-
-Ouch. Son of a pellet. You can't move.
-
-Exactly 4 hours and 3 minutes later, a foot descends onto you, splattering you across the tile.",
-:code => '',
-:options => []
-}
-
-l << {
-:name => 'Stay In Cage',
-:text => "You huddle in the corner of the cage, nibbling on a pellet and staring at your running wheel.
-
-I'd better eat more before venturing out. Wise is the hamster that doesn't starve! ...
-Or maybe I'll go for a run. That looks fun.",
-:code => '',
-:options => [:eatpellets, :runonwheel]
-}
-
-l << {
-:name => "Run On Wheel",
-:text => "You hop on, and begin to run.
-
-Suddenly, you feel the wheel loosen and detach from the cage. The wheel hits the bottom of the cage and careens into the side, shifting the entire cage off of its resting place and onto the floor. The plastic bottom shatters.
-
-Mortified and still wheelbound, you continue to run forwards until the wheel hits a wall and falls on its side. You climb out, rattled.",
-:code => "$hamster_cage_destroyed = true",
-:options => [:bedroom]
-}
-
-l << {
-:name => "Bedroom",
-:location => :bedroom,
-:text => "You're in your bedroom.
-	
-	",
-:code => "
-	if $jane_location == :bedroom || $larissa_location == :bedroom
-		@text << %(Someone's in the bedroom!)
-		@options << :putbackincage
-	else	
-		if $cat_location == :bedroom
-			@text << 'There is a cat slinking about the room, eyeing you. It makes you uncomfortable.'
-			@options << :runfromcat
+page :incage,
+	"You're in your cage. The door is ",
+	:stayincage => "Stay in cage",
+	do
+		if $hamster_cage_open
+		  @text << "open"
+		  @options.merge! << :exitcage
 		else
-			@options << :explorebedroom << :hallway
-		end	
-		
-		if $hamster_cage_destroyed
-			@text << 'Your cage lies shattered on the floor.'
-		elsif $hamster_cage_open
-			@text << 'Your cage is open.'
-			@options << :returntocage
+		  @text << "closed"
 		end
 	end
-	",
-:options => []
-}
 
-l << {
-:name => "Return To Cage",
-:text => "You scuttle back to your cage!",
+page :exitcage,
+	"You scurry out the door. On the last bar, you realize there's nothing stopping your 4 foot descent onto the tile floor. You try to cling onto metal with your hind legs, but it's no use. You plummet.
+
+	Ouch. Son of a pellet. You can't move.
+
+	Exactly 4 hours and 3 minutes later, a foot descends onto you, splattering you across the tile.",
+
+
+page :stayincage,
+	"You huddle in the corner of the cage, nibbling on a pellet and staring at your running wheel.
+	",
+	:eatpellets => "I'd better eat more before venturing out. Wise is the hamster that doesn't starve! ...", :runonwheel => "Or maybe I'll go for a run. That looks fun."
+
+
+page :runonwheel,
+	"You hop on, and begin to run.
+
+	Suddenly, you feel the wheel loosen and detach from the cage. The wheel hits the bottom of the cage and careens into the side, shifting the entire cage off of its resting place and onto the floor. The plastic bottom shatters.
+
+	Mortified and still wheelbound, you continue to run forwards until the wheel hits a wall and falls on its side. You climb out, rattled.",
+	:bedroom => "Bedroom",
+	do
+		$hamster_cage_destroyed = true
+	end
+
+page :bedroom,
+	"You're in your bedroom.
+	
+	",
+	:location => :bedroom,
+	do
+		if $jane_location == :bedroom || $larissa_location == :bedroom
+			@text << %(Someone's in the bedroom!)
+			@options.merge! :putbackincage => "Get Put Back In Cage"
+		else	
+			if $cat_location == :bedroom
+				@text << 'There is a cat slinking about the room, eyeing you. It makes you uncomfortable.'
+				@options.merge! :runfromcat => "Run from cat"
+			else
+				@options.merge! :explorebedroom => "Explore Bedroom"
+				@options.merge! :hallway => "Hallway"
+			end	
+			
+			if $hamster_cage_destroyed
+				@text << 'Your cage lies shattered on the floor.'
+			elsif $hamster_cage_open
+				@text << 'Your cage is open.'
+				@options.merge! :returntocage => "Return to cage"
+			end
+		end
+	end
+	
+page :returntocage,
+	"You scuttle back to your cage!",
 :code => "",
 :options => [:incage]
 }
 
-l << {
-:name => "Hallway",
-:location => :hallway,
-:text => "You're in the hallway.
+page: hallway,
 
-",
-:code => %^
+	"You're in the hallway.
+
+	",
+	:location => :hallway,
+	
+dFUCK
 	if $jane_location == :hallway || $larissa_location == :hallway
 		@text << %(Someone's in the hallway!)
-		@options << :putbackincage
+		@options.merge! << :putbackincage
 	else
 		if $cat_location == :hallway
 			@text << "A cat sits on the untended carpet.
 			"
-			@options << :runfromcat
+			@options.merge! :runfromcat
 		else
-			@options << :bedroom << :livingroom
+			@options.merge! :bedroom << :livingroom
 		end
 	end
 		^,
@@ -135,12 +123,12 @@ l << {
 
 		The cat is outside.]
 		
-		@options << :bedroom
+		@options.merge! << :bedroom
 		$cat_location = :outside
 	else
 		@text << %[The hand deposits you unceremoniously into your soiled confines, shutting the door behind.]
 		
-		@options << :incage
+		@options.merge! << :incage
 		$hamster_cage_open = false
 		),
 :options => []
@@ -152,9 +140,9 @@ l << {
 :code => %^
 	$hamster_pellets = 6
 	if $hamster_cage_open
-		@options << :wanderoutofcage
+		@options.merge! << :wanderoutofcage
 	else
-		@options << :stayincage
+		@options.merge! << :stayincage
 	end
 	^,
 :options => []
@@ -166,9 +154,9 @@ l << {
 :code => %^
 	if $cat_location == :bedroom
 		@text << "A cat stalks by."
-		@options << :leapontocat << :leapawayfromcat
+		@options.merge! << :leapontocat << :leapawayfromcat
 	else
-		@options << :bedroom
+		@options.merge! << :bedroom
 	end
 		^,
 :options => []
@@ -201,13 +189,13 @@ l << {
 :code => %^
 	if $jane_location == :livingroom || $larissa_location == :livingroom
 		@text << "There's someone in the room!"
-		@options << :putbackincage
+		@options.merge! << :putbackincage
 	else
 		if $cat_location = :livingroom
 			@text << "A cat perks up and stares at you from the sofa."
-			@options << :runfromcat << :approachcat
+			@options.merge! << :runfromcat << :approachcat
 		else
-			@options << :kitchen << :hallway
+			@options.merge! << :kitchen << :hallway
 		end
 	end
 		^,
@@ -230,7 +218,7 @@ l << {
 	$hamster_pellets = 0 unless $hamster_pellets
 	$hamster_pellets -= 1 if $hamster_pellets > 0
 	@text << "You now have #{$hamster_pellets}_ pellets"
-	@options << $hamster_escape	
+	@options.merge! << $hamster_escape	
 		^,
 :options => []
 }
@@ -242,7 +230,7 @@ l << {
 :code => %^
 	if $jane_location == :kitchen || $larissa_location == :kitchen
 		@text << "Someone's in the kitchen!"
-		@options << :putbackincage
+		@options.merge! << :putbackincage
 	else
 	end
 	^,
